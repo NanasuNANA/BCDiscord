fb2d14d90e7673b91e107475b340ff0eb08baec6
Nanasu NANA <nnanasu7@gmail.com>
2017-07-31 21:36:39 +0900

    ダイスコマンドに独自処理を加える機能追加。

--- config/config.js
+++ config/config.js
@@ -1,4 +1,12 @@
 const CONFIG = {
   api_url: '', //BCDice-API サーバのURL
-  lock_url_input: true //URL入力欄をロックするか（configファイルでURLが指定されている場合のみ有効）
-}
+  lock_url_input: true, //URL入力欄をロックするか（configファイルでURLが指定されている場合のみ有効）
+  //ダイスコマンドの全角半角変換後に独自処理を行う関数を登録できる
+  //dice_command_post_process: function(command='', gameInfo={}) {
+  //  if (gameInfo.gameType === 'DoubleCross') {
+  //    // gameTypeがダブルクロスの場合、ダイスのデフォルトをD10にする
+  //    return command.replace(/(^|\b)(\d+D)(D|[^\d\w]|$)/gi, "$1$210$3");
+  //  }
+  //  return command;
+  //}
+};
